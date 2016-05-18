987af1f6f9e3905b7d1a7e5f6225428fc892f892
willriggins <williamriggins2013@gmail.com>
2016-05-18 12:03:47 -0400

    Refactor

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -14,5 +14,16 @@ public class Main {
         player.chooseName();
         player.chooseWeapon();
         player.chooseLocation();
+
+        System.out.println("Type a number...");
+        String num = scanner.nextLine();
+        int numInt = Integer.valueOf(num);
+
+        if (numInt < 0) {
+            System.out.println("That's a negative number!");
+        }
+        else {
+            System.out.println("That's a positive number!");
+        }
     }
 }
